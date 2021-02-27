0fa3e743bf71780f99bfc4de9d8b7ac408f31fe2
noobmish <71914836+noobmish@users.noreply.github.com>
2021-02-27 10:45:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var canva;
+
+function setup(){
+  canvas = createCanvas(850,400);
+  sumbitButton= createButton('Sumbit')
+}
+
+
+function draw(){
+  background("pink");
+sumbitButton.postition(425,300)
+  
+}
